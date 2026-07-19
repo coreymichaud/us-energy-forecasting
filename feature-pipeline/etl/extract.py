5da11824c9a5f23f482fd5d27a6b59c46e41a335
Corey Michaud <cor3y.michaud@gmail.com>
2026-07-19 16:09:28 -0400

    Created API extraction

--- feature-pipeline/etl/extract.py
+++ feature-pipeline/etl/extract.py
@@ -1,18 +1,34 @@
 import requests
-import json
-from dotenv import load_dotenv
-import os
+import pandas as pd
+import logging
 
-load_dotenv()
+logger = logging.getLogger(__name__)
+logging.basicConfig(level = logging.INFO)
 
-EIA_API_KEY = os.getenv("EIA_API_KEY")
+def get_api_data(URL: str, START_DATE: str, API_KEY: str) -> pd.DataFrame:
 
-START_DATE = "2026-07-18T00"
+    try:
+        logger.info(" Fetching data from EIA API")
+        response = requests.get(f"{URL}&start={START_DATE}&api_key={API_KEY}")
 
-response = requests.get(
-    f"https://api.eia.gov/v2/electricity/rto/region-data/data/?frequency=hourly&data[0]=value&start={START_DATE}&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000&api_key={EIA_API_KEY}"
-)
+        data = response.json()
+        df = pd.DataFrame(data['response']['data'])
 
-data = response.json()
+    except Exception as e:
+        logger.error(f"Error fetching API data: {e}")
+        df = pd.DataFrame()
 
-print(json.dumps(data, indent=4, sort_keys=True))
+    return df
+
+# Testing
+if __name__ == "__main__":
+    from dotenv import load_dotenv
+    import os
+    load_dotenv()
+    EIA_API_KEY = os.getenv("EIA_API_KEY")
+    data = get_api_data(
+        URL = "https://api.eia.gov/v2/electricity/rto/region-data/data/?frequency=hourly&data[0]=value&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000",
+        START_DATE = "2026-07-19",
+        API_KEY = EIA_API_KEY
+    )
+    print(data.head())
