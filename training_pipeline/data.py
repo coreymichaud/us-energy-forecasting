450b9d896b4fe5415a4688231d38f112f3db28f9
Corey Michaud <cor3y.michaud@gmail.com>
2026-07-19 22:03:37 -0400

    Initial training pipeline

---
+++ training_pipeline/data.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from datetime import datetime
+from feast import FeatureStore
+
+entity_df = pd.DataFrame(
+    {
+        "event_timestamp": [pd.Timestamp(datetime.now(), tz="UTC")],
+        "driver_id": [1001]
+    }
+)
+
+feature_refs = [
+    "online_store:period",
+    "online_store:respondent",
+    "online_store:type",
+    "online_store:value"
+]
+
+fs = FeatureStore(repo_path="feature_pipeline")
+
+training_df = fs.get_historical_features(
+    features=[
+        "online_store:period",
+        "online_store:respondent",
+        "online_store:type",
+        "online_store:value"
+    ],
+    entity_df=entity_df
+).to_df()
