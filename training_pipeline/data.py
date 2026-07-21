1c40956f35eb7a0174c8e873da0ceadfba4d1c4d
Corey Michaud <cor3y.michaud@gmail.com>
2026-07-20 21:42:31 -0400

    Ruff formatted

--- training_pipeline/data.py
+++ training_pipeline/data.py
@@ -3,17 +3,14 @@ from datetime import datetime
 from feast import FeatureStore
 
 entity_df = pd.DataFrame(
-    {
-        "event_timestamp": [pd.Timestamp(datetime.now(), tz="UTC")],
-        "driver_id": [1001]
-    }
+    {"event_timestamp": [pd.Timestamp(datetime.now(), tz="UTC")], "driver_id": [1001]}
 )
 
 feature_refs = [
     "online_store:period",
     "online_store:respondent",
     "online_store:type",
-    "online_store:value"
+    "online_store:value",
 ]
 
 fs = FeatureStore(repo_path="feature_pipeline")
@@ -23,7 +20,7 @@ training_df = fs.get_historical_features(
         "online_store:period",
         "online_store:respondent",
         "online_store:type",
-        "online_store:value"
+        "online_store:value",
     ],
-    entity_df=entity_df
+    entity_df=entity_df,
 ).to_df()
